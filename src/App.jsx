17e0e4cac1d5cf940f7d16ef53f307aab34fe19f
Nanthini <nanthinisampath3009@gmail.com>
2024-08-13 19:04:09 +0530

    changes done in card

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import NavBar from "./NavBar";
 import Header from "./Header";
 import Footer from "./Footer";
-import Card from "./Card";
+import Card from "./card";
 function App() {
   let [cart, setCart] = useState(0);
   return (
